4e0306f844f080df010462af1f536e3e54b6791a
xindongni <xindongni@xindongnideiMac.local>
2016-01-07 15:56:58 +0800

    接口2

--- ISQ/Extension/TestJSObject.m
+++ ISQ/Extension/TestJSObject.m
@@ -10,7 +10,10 @@
 #import "MainViewController.h"
 #import "HMAC-SHA1.h"
 
-@implementation TestJSObject
+@implementation TestJSObject{
+
+    NSDictionary *dic2;
+}
 
 -(void)passShareParams:(NSString*)str {
     
@@ -28,6 +31,7 @@
     
     NSData *data=[str dataUsingEncoding:NSUTF8StringEncoding];
     NSDictionary *dic=[[NSDictionary alloc] init];
+    dic2 = [[NSDictionary alloc] init];
     dic =[NSJSONSerialization JSONObjectWithData:data options:NSJapaneseEUCStringEncoding error:nil];
     if (dic[@"name"]&&dic[@"action"]) {
         
@@ -38,6 +42,9 @@
         NSMutableArray *array = [NSMutableArray array];
         NSUserDefaults *userInfo = [NSUserDefaults standardUserDefaults];
         NSString *uid = [userInfo objectForKey:MyUserID];
+        
+         /****************************接口1******************************/
+        
         [array addObject:timestamp];
         [array addObject:nonce];
         [array addObject:[NSString stringWithFormat:@"name=%@",dic[@"name"]]];
@@ -49,6 +56,7 @@
             
             NSDictionary *dic = [[NSDictionary alloc] init];
             dic = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJapaneseEUCStringEncoding error:nil];
+            dic2 = dic;
             self.passBill(dic);
             
         } failure:^(NSError *erro) {
@@ -57,9 +65,32 @@
             self.passBill(nil);
 
         }];
+        
+       /****************************接口2******************************/
+        NSMutableArray *array2 = [NSMutableArray array];
+        [array2 addObject:timestamp];
+        [array2 addObject:nonce];
+        [array2 addObject:[NSString stringWithFormat:@"name=%@",@"property"]];
+        [array2 addObject:[NSString stringWithFormat:@"action=%@",@"pay_confirm"]];
+        //加密
+        NSString *signature2 = [HMAC_SHA1 hmac_sha1:@"Q2sE#FeNK8%6awIO" parames:array2 url:@"http://webapp.wisq.cn/api"];
+        NSString *url2 = [NSString stringWithFormat:@"%@?%@&%@&name=%@&action=%@&signature=%@&order_no=%@",@"http://webapp.wisq.cn/api",timestamp,nonce,@"property",@"pay_confirm",signature2,dic2[@"order_no"]];
+        [ISQHttpTool getHttp:url2 contentType:nil params:nil success:^(id responseObject) {
+            
+            NSDictionary *dic = [[NSDictionary alloc] init];
+            dic = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJapaneseEUCStringEncoding error:nil];
+            NSLog(@"dicc ----%@",dic);
+            
+        } failure:^(NSError *erro) {
+            
+            NSLog(@"error--%@",erro);
+        }];
+
+        
     }
     
 }
+
 
 
 @end
