2ef288415acada574028101003ad842f91415d41
wangmingmin <498373454@qq.com>
2016-02-03 11:25:25 +0800

    修改注册时我已阅读协议，访问服务器上的协议，不再直接加载本地html

--- ISQ/UserAgreementController.m
+++ ISQ/UserAgreementController.m
@@ -7,8 +7,7 @@
 //
 
 #import "UserAgreementController.h"
-
-@interface UserAgreementController ()
+@interface UserAgreementController ()<UIWebViewDelegate>
 
 @end
 
@@ -35,20 +34,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
-   
-    
+    NSString * htmlStr = @"http://webapp.wisq.cn/User/agreement.html";
+    NSURL * url = [NSURL URLWithString:htmlStr];
+    NSURLRequest * request = [NSURLRequest requestWithURL:url];
+    self.navigationController.navigationBar.hidden=YES;
+    [self.agreementWebview loadRequest:request];
+    self.agreementWebview.delegate = self;
+}
+
+-(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
     self.ageemenTitle_label.text=_useAgreeData[0];
-    
-    
     
     NSString *filePath = [[NSBundle mainBundle]pathForResource:[NSString stringWithFormat:@"%@",_useAgreeData[0]] ofType:@"html"];
     NSString *htmlString = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
     [self.agreementWebview loadHTMLString:htmlString baseURL:[NSURL URLWithString:filePath]];
-    
-    
+
 }
 
+-(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    if ([request.mainDocumentURL.relativeString rangeOfString:@"isq_back_native"].location !=NSNotFound){
+        self.navigationController.navigationBar.hidden=NO;
+        [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationSlide];
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+    return YES;
+}
+
+-(void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
